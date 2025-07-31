be5d43ae59ee75e636d3ae436af5f191f953b535
musarri <msaricicek_99@hotmail.com>
2025-07-31 18:35:46 +0300

    breadcrumb removed

--- src/layout/DashboardLayout/index.jsx
+++ src/layout/DashboardLayout/index.jsx
@@ -64,7 +64,7 @@ export default function DashboardLayout({ children }) {
             flexDirection: 'column'
           }}
         >
-          <Breadcrumbs />
+          {/* <Breadcrumbs /> */}
           {children}
           <Footer />
         </Container>
